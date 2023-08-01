f09578b8ef5cee5913f5fcbd68442040825ce4e7
Tornike-Peradze <t_peradze@cu.edu.ge>
2023-08-01 13:47:07 +0400

    js custom input functionality addition

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ let peopleAmount = document.querySelectorAll("#people-amount");
 let amountPerPerson = document.querySelectorAll("#amount-persona");
 let totalTip = document.querySelectorAll("#total-tip");
 let resetButton = document.querySelectorAll(".reset");
+
+const numberPattern = /^\d+(\.\d+)?$/;
+
+tipCustom.addEventListener("input", () => {
+  const inputValue = tipCustom.value;
+  if (numberPattern.test(inputValue)) {
+    console.log("Valid custom tip:", inputValue);
+  } else {
+    console.log("Invalid custom tip:", inputValue);
+  }
+});
+
+tipExamples.forEach((button) => {
+  button.addEventListener("click", () => {
+    tipExamples.forEach((btn) => btn.classList.remove("active"));
+    // This line removes the "active" class from all buttons before adding it to the clicked button.
+    button.classList.add("active");
+
+    if (button.id === "custom") {
+    } else {
+      const selectedTipPercentage = parseInt(button.textContent);
+    }
+  });
+});
