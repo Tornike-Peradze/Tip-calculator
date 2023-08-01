4e61b8ad1594933eefbc64dec8ce1434cd092c90
Tornike-Peradze <t_peradze@cu.edu.ge>
2023-08-01 14:24:04 +0400

    js changes

--- main.js
+++ main.js
@@ -1,30 +1,11 @@
-let billAmount = document.querySelectorAll("#bill");
+let billAmount = document.querySelector("#bill");
 let tipExamples = document.querySelectorAll(".tip-example");
-let tipCustom = document.querySelectorAll("#custom");
-let peopleAmount = document.querySelectorAll("#people-amount");
-let amountPerPerson = document.querySelectorAll("#amount-persona");
-let totalTip = document.querySelectorAll("#total-tip");
-let resetButton = document.querySelectorAll(".reset");
-
-tipExamples.forEach((button) => {
-  button.addEventListener("click", () => {
-    tipExamples.forEach((btn) => btn.classList.remove("selected"));
-    // This line removes the "active" class from all buttons before adding it to the clicked button.
-    button.classList.add("selected");
-  });
-});
-
-const numberPattern = /^\d+(\.\d+)?$/;
-
-tipCustom.addEventListener("input", () => {
-  const inputValue = tipCustom.value;
-  if (numberPattern.test(inputValue)) {
-    customTipValue = parseFloat(inputValue);
-    tipExamples.forEach((btn) => btn.classList.remove("selected"));
-  } else {
-    customTipValue = null;
-  }
-});
+let tipCustom = document.querySelector("#custom");
+let peopleAmount = document.querySelector("#people-amount");
+let amountPerPerson = document.querySelector("#amount-persona");
+let totalTip = document.querySelector("#total-tip");
+let resetButton = document.getElementById("#reset");
+console.log(resetButton);
 
 let selectedTipPercentage = 0;
 
@@ -32,4 +13,44 @@ let customTipValue = null;
 
 function calculateTip() {
   // Get the bill amount and number of people
+  const bill = parseFloat(billAmount.value);
+  const numOfPeople = parseFloat(peopleAmount.value);
+  console.log(numOfPeople);
+  // Calculate tip amout based on the selected percentage or custom value
+  const tipAmount =
+    customTipValue !== null
+      ? customTipValue
+      : (bill * selectedTipPercentage) / 100;
+
+  // Calculate total per person (including tip)
+  const totalPerPerson = (bill + tipAmount) / numOfPeople;
+
+  amountPerPerson.textContent = `$${totalPerPerson.toFixed(2)}`;
+  totalTip.textContent = `$${tipAmount.toFixed(2)}`;
 }
+
+// Add event listeners to tip percentage buttons
+tipExamples.forEach((button) => {
+  button.addEventListener("click", () => {
+    selectedTipPercentage = parseFloat(button.textContent);
+    // Remove the "selected" class from all buttons
+    tipExamples.forEach((btn) => btn.classList.remove("selected"));
+    // Add the "selected" class to the clicked button
+    button.classList.add("selected");
+    // Calculate tip amount and total per person
+    calculateTip();
+  });
+});
+
+// Add event listener to reset button
+resetButton.addEventListener("click", () => {
+  // Reset input values and displayed values
+  billAmount.value = "";
+  tipExamples.forEach((btn) => btn.classList.remove("selected"));
+  tipCustom.value = "Custom";
+  peopleAmount.value = "";
+  amountPerPerson.textContent = "$0.00";
+  totalTip.textContent = "$0.00";
+  // Reset custom tip value
+  customTipValue = null;
+});
